70d038f7a71b0de5d8e7f48ed855978c5e607bcb
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2019-09-04 14:49:21 +0200

    Added more functionality.
    Can now Withdraw, see balance and deposit.

--- Solutions/BankAssignment/BankClient/Form1.cs
+++ Solutions/BankAssignment/BankClient/Form1.cs
@@ -12,10 +12,29 @@ namespace BankClient
 {
     public partial class Form1 : Form
     {
+
+        BankService bankService = new BankService();
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void checkBalance_button_Click(object sender, EventArgs e)
+        {
+            fundsValue.Text = bankService.CheckBalance(accountNoTextbox.Text, 0);
+        }
+
+        private void DepositButton_Click(object sender, EventArgs e)
+        {
+            bankService.AddFunds(accountNoTextbox.Text, fundsInput.Text);
+            fundsValue.Text = bankService.CheckBalance(accountNoTextbox.Text, 0);
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            bankService.WithdrawFunds(accountNoTextbox.Text, fundsInput.Text);
+            fundsValue.Text = bankService.CheckBalance(accountNoTextbox.Text, 0);
+        }
     }
 }
---
+++ Solutions/BankAssignment/BankClient/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankClient
+{
+    class Account
+    {
+        public Account(string accountNo)
+        {
+            AccountNo = accountNo;
+        }
+
+        public string AccountNo { get; set; }
+    }
+}
