ca101431c99f000c6a946728a9c2681b9819051b
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2019-09-11 20:32:22 +0200

    RPC Solution added.

---
+++ Solutions/System Integration/RPCAssigment/RPCServer/RPCAssigment/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCAssignment.Models
+{
+    class Student
+    {
+        // Basic model class
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+
+    }
+}
---
+++ Solutions/System Integration/RPCAssigment/RPCServer/RPCAssigment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Grpc.Core;
+
+namespace RPCAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int Port = 6789;
+            var server = new Server
+                {
+                    Services = { StudentService.BindService(new ServerImplementation()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+
+                Console.WriteLine("Greeter server listening on port " + Port);
+                Console.WriteLine("Press any key to stop the server...");
+                Console.ReadKey();
+
+                server.ShutdownAsync().Wait();
+        }
+    }
+}
