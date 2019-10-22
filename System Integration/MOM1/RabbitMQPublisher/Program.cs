aea18d33583f7e5cf7100e17402800341d4ce180
Nikolaj Dyring <nikodyring@gmail.com>
2019-10-22 20:26:21 +0200

    MOM1 Assignment added.

---
+++ System Integration/MOM1/RabbitMQPublisher/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQPublisher
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "Campaign", type: ExchangeType.Direct);
+            var campaign = "";
+            while (campaign != "break")
+            {
+                Console.WriteLine("Select a car class to promote. \n" +
+                                  "A, B or C");
+                campaign = Console.ReadLine().ToLower();
+                var message = GetAdvertisement(campaign);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "Campaign",
+                    routingKey: $"{campaign}",
+                    basicProperties: null,
+                    body: body);
+                Console.WriteLine($"[x] Sent {message} to customers wanting the {campaign}-campaign.");
+            }
+        }
+
+        private static string GetAdvertisement(string campaign)
+        {
+            switch (campaign)
+            {
+                default:
+                    {
+                        return "Dreams come true this autumn, rent a Fiat 500 to go places. Only 30$ a day!";
+                    }
+
+                case "a":
+                    {
+                        return "(A)lpha week has started! Get our Tesla Model S for only 299.000,99$ a year!";
+                    }
+                case "b":
+                    {
+                        return "We're currently rolling out our (B)ig Campaign! Rent a Ford Fiesta for only 129.99$ a day!";
+                    }
+                case "c":
+                    {
+                        return "We've got an excellent offer on a Citroën C1, only 99.99$ a day!";
+                    }
+            }
+        }
+    }
+}
---
+++ System Integration/MOM1/RabbitMQReciever/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQReceiver
+{
+    class Program
+    {
+        public static event Action RaiseChoice;
+        static void Main(string[] args)
+        {
+            List<string> interestList = new List<string>();
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                RaiseChoice += Choice;
+                channel.ExchangeDeclare(exchange: "Campaign", type: ExchangeType.Direct);
+                Console.WriteLine("Welcome to Rent-A-Phat-Car! \n" +
+                                  "___________________________ \n" +
+                                  "Which class of car are you interested in renting?");
+                string interest = Console.ReadLine();
+
+                interestList.Add(interest);
+                interestList.Add("");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                foreach (var v in interestList)
+                {
+                    channel.QueueBind(queue: queueName,
+                        exchange: "Campaign",
+                        routingKey: $"{v}");
+                }
+
+
+                Console.WriteLine("[*] Waiting for offers.");
+                while (true)
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(message);
+                            //new Task(Choice).Start();
+                            //RaiseChoice?.Invoke();
+                            Choice();
+
+                    };
+                    channel.BasicConsume(queue: queueName,
+                        autoAck: true,
+                        consumer: consumer);
+
+                }
+            }
+
+        }
+
+        private static void Choice()
+        {
+            Console.WriteLine("Would you like to confirm this booking? [C]onfirm or [P]ass");
+
+            string answer = Console.ReadLine().ToUpper();
+            if (answer == "C")
+            {
+                Console.WriteLine("Booking confirmed.");
+            }
+            else if (answer == "P")
+            {
+                Console.WriteLine("Booking has been passed.");
+            }
+        }
+    }
+}
